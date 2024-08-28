0289415308b296e6cbc35a687cb246a99a1d8b30
AbibshdQ <habib.maushid69@gmail.com>
2024-08-28 14:38:00 +0700

    c

---
+++ database/migrations/2024_08_28_073153_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id'); 
+            $table->string('name',50);
+            $table->string('gender',10);
+            $table->date('day_of_birth');
+            $table->string('place_of_birth', 50);
+            $table->string('adress',100);
+            $table->string('status', 25);
+            $table->date('entry_date');
+            // $table->unsignedInteger('id_jabatan'); // Merujuk ke kolom 'id' di tabel 'jabatans'
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
